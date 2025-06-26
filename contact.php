3e39f41c93b9a9258874c9b3ab27d6ab1040a589
Shaine Esquivel <shaineesquivel@gmail.com>
2025-06-26 17:27:25 +0800

    esquivel

--- contact.php
+++ contact.php
@@ -9,9 +9,48 @@
     <link rel="icon" type="img/png" href="img/FINAL.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   </head>
-  <body>
+  <body class="contact">
 
       <?php include 'navbar.php'; ?>  
 
+
+    <section class="contact-section">
+    <h1 class="contact">Meet the Team</h1>
+    <div class="team-container">
+
+      <div class="team-member">
+        <img src="img/arellano.jpg" alt="Arellano">
+        <h3><strong>ARELLANO,</strong> Francis Miguel</h3>
+        <p>Email: arellanofrancis321@gmail.com</p>
+        <p>Contact: 0998 234 4600</p>
+      </div>
+
+      <div class="team-member">
+        <img src="img/chan.jpg" alt="Chan">
+        <h3><strong>CHAN,</strong> Justine Henry</h3>
+        <p>Email: justinechan.05@gmail.com</p>
+        <p>Contact: 0966 548 5388</p>
+      </div>
+
+      <div class="team-member">
+        <img src="img/esquivel-s.jpg" alt="Esquivel">
+        <h3>ESQUIVEL, Shaine</h3>
+        <p>Email: shaineesquivel@gmail.com</p>
+        <p>Contact: 0929 117 3617</p>
+      </div>
+
+      <div class="team-member">
+        <img src="img/guevarra.jpg" alt="Guevarra">
+        <h3>GUEVARRA, Clide Ivans</h3>
+        <p>Email: clide2905@gmail.com</p>
+        <p>Contact: 0991 426 4963</p>
+      </div>
+
+      <div class="team-member">
+        <img src="img/hernandez.jpg" alt="Hernandez">
+        <h3>HERNANDEZ, John Matthew</h3>
+        <p>Email: john.matthew.hernandez@gmail.com</p>
+        <p>Contact: 0933 445 2211</p>
+      </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
   </body>
