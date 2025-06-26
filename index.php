5ce01ae5a20ced40db8e2342608e0d7d05c7fc4c
unknown <matthewehernandez0712@gmail.com>
2025-06-26 13:02:38 +0800

    hernandez

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
       <div class="row m-3 py-5 justify-content-center">
        
         <div class="col-md-9 d-flex flex-column">
-          <div class="carousel slide" data-bs-ride="carousel">
+          <div id="CarouselSlide" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="3000">
               <img src="img/quote 1.jpg" class="d-block w-100" alt="quote 1">
@@ -89,11 +89,11 @@
               <img src="img/quote 4.jpg" class="d-block w-100" alt="quote 4">
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <button class="carousel-control-prev" type="button" data-bs-target="#CarouselSlide" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
           </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <button class="carousel-control-next" type="button" data-bs-target="#CarouselSlide" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
