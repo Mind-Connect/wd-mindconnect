81bf8c473193db4aac88b300aac6c0a7d76557bf
Shaine Esquivel <shaineesquivel@gmail.com>
2025-06-26 14:33:37 +0800

    resolve merge conflict in index.php and style.css, esquivel

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <title>Mind Connect</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inria+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <link rel="icon" type="img/png" href="img/FINAL.png">
   </head>
@@ -50,7 +52,7 @@
       <div class="hero-overlay"></div>
       <div class="container hero-content">
         <h5>Caring for a better life</h5>
-        <h1 style="color: rgb(0,0,0);">Your Mental Health Matters.</h1>
+        <h1 style="color: rgb(255, 255, 255);">Your Mental Health Matters.</h1> 
         <p>
           Take time to check in with yourself. It's okay to pause, to breathe, and to ask for help when you need it. You don't have to go through anything alone — support is always within reach. Caring for your mental well-being is not a sign of weakness, but a step toward strength. You deserve peace, healing, and support.
         </p>
@@ -74,6 +76,7 @@
       <div class="row m-3 py-5 justify-content-center">
        
         <div class="col-md-9 d-flex flex-column">
+          <h2 class="text-center mb-4">Words you didn’t ask for, but maybe needed.</h2>
           <div id="CarouselSlide" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="3000">
@@ -97,8 +100,7 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
-        </div>
-      </div>
+      
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
