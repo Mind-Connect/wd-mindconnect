ffc1b9f8231a1795abb2273be854682f2c47c3e6
unknown <matthewehernandez0712@gmail.com>
2025-06-26 13:51:51 +0800

    hernandez

--- about.php
+++ about.php
@@ -11,37 +11,9 @@
   </head>
   <body>
 
-    <div class="container-fluid shadow" style="background:#205375;">
-      <div class="container">
-        <div class="row">
-          <div class="col py-3">
-            <div class="d-flex flex-row justify-content-between align-items-center">
-              <div class="d-flex flex-row justify-content-start align-items-center">
-                <a class="navbar-brand text-white d-flex align-items-center" href="#">
-                  <img src="img/FINAL.png" alt="Logo">
-                  Mind Connect
-                </a>
-              </div>
-              <div class="d-flex flex-row justify-content-end align-items-center">
-                <ul class="navbar-nav ms-auto d-flex flex-row">
-                  <li class="nav-item">
-                    <a class="nav-link text-white" href="index.php">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link text-white" href="about.php">About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link text-white" href="contact.php">Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      <?php include 'navbar.php'; ?>  
 
-    <section id="about" class="bg-muted">
+    <section id="about" class="bg-muted my-5">
         <div class="container">
             <div class="section-header">
                 <h1 class="section-title">About Us</h1>
---
+++ navbar.php
@@ -0,0 +1,29 @@
+<div class="container-fluid shadow fixed-top" style="background:rgb(32, 83, 117);">
+      <div class="container">
+        <div class="row">
+          <div class="col py-3">
+            <div class="d-flex flex-row justify-content-between align-items-center">
+              <div class="d-flex flex-row justify-content-start align-items-center">
+                <a class="navbar-brand text-white d-flex align-items-center" href="#">
+                  <img src="img/FINAL.png" alt="Logo">
+                  Mind Connect
+                </a>
+              </div>
+              <div class="d-flex flex-row justify-content-end align-items-center">
+                <ul class="navbar-nav ms-auto d-flex flex-row">
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="about.php">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="contact.php">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
